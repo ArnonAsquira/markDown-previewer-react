2982f5745dfa5c8a6195d1d3e54ecf7642136687
ArnonAsquira <arnonafking2010@gmail.com>
2021-11-19 01:17:41 +0200

    creating a simple boiler plate to be used in future react projects

---
+++ mark-down-previewer/src/index.js
@@ -0,0 +1,13 @@
+import './css/main.css';
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class Main extends React.Component {
+
+
+    render() {
+        return null
+    }
+} 
+
+ReactDOM.render(Main, document.getElementById('root'));
